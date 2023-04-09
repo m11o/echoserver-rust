af86420cde268a7406aedaabb04a76d68da3b5f1
m11o <m11o@hey.jp>
2023-04-09 22:59:11 +0900

    add pre socket

--- src/socket.rs
+++ src/socket.rs
@@ -10,3 +10,38 @@ use std::net::{IpAddr, Ipv4Addr};
 use std::time::SystemTime;
 
 const SOCKET_BUFFER_SIZE: usize = 4380;
+
+#[derive(Debug, Hash, Eq, PartialEq, Clone, Copy)]
+pub struct SockID(pub Ipv4Addr, pub Ipv4Addr, pub u16, pub u16);
+
+pub struct Socket {
+    pub local_addr: Ipv4Addr,
+    pub remote_addr: Ipv4Addr,
+    pub local_port: u16,
+    pub remote_port: u16,
+    pub sender: TransportSender,
+}
+
+pub enum TcpStatus {}
+
+impl Socket {
+    pub fn new(
+        local_addr: Ipv4Addr,
+        remote_addr: Ipv4Addr,
+        local_port: u16,
+        remote_port: u16,
+        sender: TransportSender,
+    ) -> Result<Self, ()> {
+        let (sender, _) = transport::transport_channel(
+            65535,
+            TransportChannelType::Layer4(TransportProtocol::Ipv4(IpNextHeaderProtocols::Tcp)),
+        )?;
+        Ok(Self {
+            local_addr,
+            remote_addr,
+            local_port,
+            remote_port,
+            sender,
+        })
+    }
+}
